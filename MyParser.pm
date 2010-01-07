6e46b2f1f1c24109fb2cdcd806bd101275fe97b8
Zakaria Elqotbi <zakaria.elqotbi@openwide.fr>
2010-01-07 14:03:48 +0100

    initialisation GetTransport project

---
+++ MyParser.pm
@@ -0,0 +1,177 @@
+#!/usr/bin/perl -w
+# get Result link 
+
+package MyParser; 
+
+use strict;
+use Switch;
+use HTML::Parser 3.00 ();
+use Exporter;
+
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+$VERSION     = 1.00;
+@ISA         = qw(Exporter);
+@EXPORT      = ();
+@EXPORT_OK   = qw(get_resultlink phpsession);
+%EXPORT_TAGS = ( DEFAULT => [qw(&get_resultlink)],
+                 Both    => [qw(&get_resultlink &phpsession)]);
+
+
+my %inside;
+my $URL;
+my $resultURL;
+my $pattern = "cliquez-ici";
+my $datavar;
+my $var;
+my $ligne;
+
+sub handle_resultlink
+{
+   my($tag,$attr, $num) = @_;
+   $inside{$tag} += $num;
+   $URL = $attr->{href};
+}
+
+sub text_resultlink
+{
+    return if $inside{script} || $inside{style};
+    if (lc($_[0]) eq $pattern )
+    {
+      #print $_[0] . "\n";
+#      print "$URL\n";
+      $resultURL = $URL;
+    }
+}
+
+sub handle_phpsession
+{
+    my($self, $tag, $attr) = @_;
+    return unless $tag eq "form";
+    return unless exists $attr->{action};
+    $URL = "$attr->{action}" ;
+}
+
+sub get_resultlink
+{
+  HTML::Parser->new(api_version => 3,
+		  handlers    => [start => [\&handle_resultlink, "tagname, attr,'+1'"],
+				  end   => [\&handle_resultlink, "tagname, attr,'-1'"],
+				  text  => [\&text_resultlink, "dtext"],
+				 ],
+		  marked_sections => 1,
+	)->parse_file(shift) || die "Can't open file: $!\n";;
+  return $resultURL;
+}
+
+sub phpsession
+{
+
+  my $p = HTML::Parser->new(api_version => 3,
+        start_h => [\&handle_phpsession, "self,tagname,attr"],
+        report_tags => [qw(form)],
+        );
+
+  $p->parse_file(shift || die) || die $!;
+
+  my $iIndex = index($URL, "PHPSESSID"); 
+  my $sNewString = substr($URL, $iIndex );
+  my @words = split /=/, $sNewString;
+  return "$words[1]";
+}
+
+# a start event for getting data
+sub handler_data
+{
+    my($self, $tag, $attr) = @_;
+    return if $tag ne "p";
+    return unless exists $attr->{class};
+    switch ( $datavar ){
+        case "depart" { return if $attr->{class} ne "pdepart" }
+        case "arrivee" { return if $attr->{class} ne "parrive" }
+        case "date"   { return if $attr->{class} ne "pdate" }
+    }
+
+    $self->handler(text => sub { $var=shift }, "dtext");
+    $self->handler(end  => sub { shift->eof if shift eq "p"; },
+                           "tagname,self");
+}
+
+# handler for the metro line parser 
+sub handle_ligne
+{
+    my($tag,$attr) = @_;
+    return unless $tag eq "img";
+    return unless exists $attr->{src};
+    #  search for pattern : ex : /picts/moteur/ligne/b68.gif
+    #  p_rer_c
+    #  tram_t3
+    #  b68
+    #  m14
+    if ( $attr->{src} =~ m/\/picts\/moteur\/ligne\/(((b|m)\d\d)|p_(rer_\D)|tram_(t\d))/ )
+    {
+        if ($2) { 
+            $ligne = $2;
+        }
+        elsif ($4){
+          $ligne = $4;
+        }
+        elsif ($5){
+            $ligne = $5;
+        }
+        #print "$ligne: $attr->{src} \n";
+    } 
+}
+
+# internal function for getting metro/bus number line .
+sub get_ligne
+{
+  HTML::Parser->new(api_version => 3,
+                    start_h => [\&handle_ligne, "tagname, attr, "],
+                    report_tags => [qw(img)],
+                    marked_sections => 1,
+  )->parse_file(shift) || die "Can't open file: $!\n";;
+  return $ligne;
+}
+
+sub get_time
+{
+     my $filename = shift;
+     my $key = shift;
+
+     $datavar = $key;
+  
+     my $p = HTML::Parser->new(api_version => 3);
+     $p->handler( start => \&handler_data, "self,tagname,attr");
+     $p->parse_file($filename || die) || die $!;
+
+     #format   xxhxx  xx/xx/xxxx 
+     if ($var=~ m/(\d*h\d*|\d*\/\d*\/\d*)/)
+     {
+         return $1;
+#         $data{$key}=$1;
+     }
+}
+
+
+# function for return data result
+sub result
+{
+  my $filename = shift;
+  my %data=();
+  
+  $data{ 'date' } = '';
+  $data{ 'depart' } = '';
+  $data{ 'arrivee' } = '';
+  $data{ 'ligne' } = '';
+  
+  for my $key qw( date depart arrivee ){
+      $data{$key} = get_time($filename, $key);
+  }
+    # getting transport line separtely
+  $data{ 'ligne' } = get_ligne($filename);
+  
+  return %data;
+}
+
+1;
---
+++ getTransport.pl
@@ -0,0 +1,108 @@
+#!/usr/bin/perl -w
+# a perl script interface to ratp.fr
+# for getting transport time/lines in paris.
+# zakaria.elqotbi zelqotbi@gmail.com
+# TODO : errors management. 
+#      : support of stations search 
+#      : adresse/stations as arguments
+#
+
+BEGIN {
+push @INC,"/home/zakaria/bin";
+    }
+
+use strict;
+use MyParser;
+
+my $initURL = "http://www33.ratp.info/Pivi/piviweb.php?exec=piviweb&cmd=NouvelleRecherche&Profil=RATP";
+my $requestURL = "http://www33.ratp.info/Pivi/piviweb.php?exec=piviweb&cmd=Lexico&Profil=RATP";
+my $resultURL = "http://www33.ratp.info/Pivi/piviweb.php?exec=piviweb&cmd=FeuilleDeRoute&Profil=RATP";
+
+my $DEPART = "14+Rue+Gaillon+PARIS-02EME";
+
+# test of RER
+#my $ARRIVEE = "Impasse+de+la+Chapelle+MONTLHERY";
+# test of metro/bus
+my $ARRIVEE = "40+Rue+d%27+Alesia+PARIS-14EME";
+# test of Tramway
+#my $DEPART="17+Boulevard+Jourdan+PARIS-14EME";
+#my $ARRIVEE="1+Boulevard+Brune+PARIS-14EME";
+
+my $Annee = "2009"; 
+my $Mois = "10";
+my $Jour = "6";
+
+my $Heure = "18";
+my $Minute = "00";
+
+my $CRITERE = "PLUS_RAPIDE"; # available criteres : 
+my $MODE = "TOUS_MODE";      # available modes:
+my $TypeDate = "DATE_DEPART"; # available types:
+
+
+my $numArgs = $#ARGV + 1;
+# if no arguement is given, we try to get localtime
+if ( $numArgs < 1 )
+{
+    my @timeData = localtime(time);
+    $Annee = $timeData[5] + 1900; 
+    $Mois = $timeData[4] + 1;
+    $Jour = $timeData[3];
+
+    $Heure = $timeData[2];
+    $Minute = $timeData[1];
+}
+# else we take the time gived as an arguments
+else {
+    my @token = split(/h/,$ARGV[0]);
+    
+    $Heure = $token[0];
+    $Minute = $token[1];
+}
+
+#my $REQUEST = "Depart%5BType%5D=RUE&Depart%5BSaisie%5D=14+Rue+Gaillon+PARIS-02EME&listePersoDepart=0&Arrivee%5BType%5D=RUE&Arrivee%5BSaisie%5D=40+Rue+d%27+Alesia+PARIS-14EME&listePersoArrivee=0&ModeTransport%5BModeTransportSelectionne%5D=TOUS_MODE&ModeTransport%5BCritereSelectionne%5D=PLUS_RAPIDE&Date%5BJourSelectionne%5D=8&Date%5BMoisSelectionne%5D=9&Date%5BAnneeSelectionne%5D=2009&Date%5BTypeDateSelectionne%5D=DATE_DEPART&Date%5BHeureSelectionne%5D=13&Date%5BMinuteSelectionne%5D=45" ;
+
+my $REQUEST = "Depart%5BType%5D=RUE&Depart%5BSaisie%5D=" . $DEPART . "&listePersoDepart=0&Arrivee%5BType%5D=RUE&Arrivee%5BSaisie%5D=" . $ARRIVEE . 
+              "&listePersoArrivee=0&ModeTransport%5BModeTransportSelectionne%5D=" . $MODE . "&ModeTransport%5BCritereSelectionne%5D=" . $CRITERE .
+              "&Date%5BJourSelectionne%5D=" . $Jour . "&Date%5BMoisSelectionne%5D=" . $Mois . "&Date%5BAnneeSelectionne%5D=" . $Annee . 
+              "&Date%5BTypeDateSelectionne%5D=" . $TypeDate . "&Date%5BHeureSelectionne%5D=" . $Heure . "&Date%5BMinuteSelectionne%5D=" . $Minute
+              ;
+
+# initialising the connection to ratp.fr and getting the php session id.
+my @curlinit = ('/usr/bin/curl','-#', "$initURL", '-o', "/tmp/initcurl.html" , '-m' , "5" );
+system @curlinit;
+
+my $PHPSESSID = MyParser::phpsession("/tmp/initcurl.html");
+
+my $requestPage = $requestURL . "&PHPSESSID=" . $PHPSESSID ;
+
+#print "$requestPage\n" ;
+
+# submitting the request :
+
+#curl -d "Depart%5BType%5D=RUE&Depart%5BSaisie%5D=14+Rue+Gaillon+PARIS-02EME&listePersoDepart=0&Arrivee%5BType%5D=RUE&Arrivee%5BSaisie%5D=40+Rue+d%27+Alesia+PARIS-14EME&listePersoArrivee=0&ModeTransport%5BModeTransportSelectionne%5D=TOUS_MODE&ModeTransport%5BCritereSelectionne%5D=PLUS_RAPIDE&Date%5BJourSelectionne%5D=8&Date%5BMoisSelectionne%5D=9&Date%5BAnneeSelectionne%5D=2009&Date%5BTypeDateSelectionne%5D=DATE_DEPART&Date%5BHeureSelectionne%5D=13&Date%5BMinuteSelectionne%5D=45"  'http://www33.ratp.info/Pivi/piviweb.php?exec=piviweb&cmd=Lexico&Profil=RATP&PHPSESSID=2beec3e8a699edaf95f5f8dec023a267'
+#curl -d $REQUEST $requestPage
+
+my @curlreq = ('/usr/bin/curl', '-#', '-d', "$REQUEST", "$requestPage", '-o', "/tmp/requestcurl.html" , '-m' , "5");
+system @curlreq;
+
+my $resultPage = MyParser::get_resultlink("/tmp/requestcurl.html");
+
+# getting the result :
+#
+#curl 'http://www33.ratp.info/Pivi/piviweb.php?exec=piviweb&cmd=FeuilleDeRoute&Profil=RATP&PHPSESSID=2beec3e8a699edaf95f5f8dec023a267' > ratp.result.html
+
+my @curlres = ('/usr/bin/curl', '-#', "$resultPage", '-o', "/tmp/resultcurl.html" , '-m' , "5");
+system @curlres;
+
+# parsing the result : 
+
+my %data = MyParser::result("/tmp/resultcurl.html");
+print "RATP info JOB->Home : ";
+while ( my ($key, $value) = each(%data) )
+{
+        print "$key => $value ";
+}
+
+print "\n";
+
